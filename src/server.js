1e5d701cdeb013f7ebfb998037e9170187ae28b3
UKP <ukpzoom4@gmail.com>
2024-08-15 23:47:29 +0530

    update server

--- src/server.js
+++ src/server.js
@@ -1,6 +1,7 @@
 const app = require("./app")
 require("dotenv").config();
 
+//start server
 app.listen(process.env.PORT, () => {
     console.log(`server is listening on: http://localhost:${process.env.PORT}`);
 })
