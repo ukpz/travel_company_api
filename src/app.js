de85a849b285dc0292cb4b0a44e981b99fb6a076
UKP <ukpzoom4@gmail.com>
2024-08-17 18:49:26 +0530

    added cors

--- src/app.js
+++ src/app.js
@@ -1,8 +1,10 @@
 const express=require("express");
+const cors=require("cors");
 const app=express();
 require("./config/dbConnection")()
 
 app.use(express.json());
+app.use(cors())
 app.use('/api',require("./api/index"))
 
 // base url
